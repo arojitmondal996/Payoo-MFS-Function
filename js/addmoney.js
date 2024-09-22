4b8de5413f069c16dcebd2c81857f88a420b1343
arojitmondal996e <arojitmondal996@gmail.com>
2024-09-22 22:31:17 +0600

    first step complete

--- js/addmoney.js
+++ js/addmoney.js
@@ -5,13 +5,23 @@
  * 3. value
  * 4. parseFloat
  */
-console.log('add money added');
+// console.log('add money added');
 document.getElementById('btn-add-money')
     .addEventListener('click', function (event) {
         event.preventDefault();
 
-        const addMoney = document.getElementById('input-add-money').ariaValueMax;
-        const addMoneyNumber = parseFloat(addMoney);
+        // const addMoney = document.getElementById('input-add-money').ariaValueMax;
+        // const addMoneyNumber = parseFloat(addMoney);
 
+        console.log('add money button clicked');
 
-    })
+        // getInputFieldValueById();
+        // const addMoney = getInputFieldValueById();
+        // console.log('add money value', addMoney);
+
+        const addMoney = getInputFieldValueById('input-add-money');
+        const pinNumber = getInputFieldValueById('input-pin-number');
+        console.log('add money with parameter', addMoney, pinNumber)
+    
+    
+    });
--- js/utilitise.js
+++ js/utilitise.js
@@ -3,8 +3,15 @@
  * 
  */
 
-console.log('utilitise added');
+// console.log('utilitise added');
 
-function getInputFieldValueById(){
-    console.log('will get value by id');
+// function getInputFieldValueById(){
+//     console.log('will get value by id');
+//     const addMoney = document.getElementById('input-add-money').value;
+//     return addMoney;
+// }
+
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
 }
